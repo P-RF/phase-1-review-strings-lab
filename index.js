584ac0a263b363445027b1ba93870bbcb2e9879b
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2025-01-14 10:52:02 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // Write your code in this file!
-const currentUser = 'Oso';
+// Define 'currentUser'
+const currentUser = 'Pri';
 
+// 'welcomeMessage' contains "Welcome to Flatbook, "
+// 'welcomeMessage' contains 'currentUser' name
+// welcomeMessage contains an exclamation point
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
+// 'excitedWelcomeMessage' contains 'welcomeMessage' capitalized
+// 'excitedWelcomeMessage' contains 'currentUser' variable
+// ends with an exclamation point
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
+// 'shortGreeting' contains 'welcomeMessage' lower cased
+// 'shortGreeting' contians first initial of the name stored in the 'currentUser' variable
+// ends with an exclamation point
 const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
